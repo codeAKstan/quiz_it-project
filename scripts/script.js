7a98c064eeb2ec127edf55c6c3f60fe31600458a
codeAKstan <boystanismoses@gmail.com>
2024-05-31 15:31:49 +0100

    Modified the code for mobile view

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show');
+    menuToggle.classList.toggle('open');
+});
